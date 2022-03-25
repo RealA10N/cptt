17f413cb8a6a0b2cd5a60a3b33a6a2231705e9dc
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-26 02:10:30 +0300

    ✨ Added token validator

--- cptt/error.py
+++ cptt/error.py
@@ -1,11 +1,26 @@
 from __future__ import annotations
 
-from cmath import inf
 from dataclasses import dataclass
 
 
 @dataclass
 class TestingError:
     code: str  # a short unique code that that represents the error type
-    priority: int = inf  # positive, 0 is highest priority
-    message: str = None
+    priority: int  # positive, 0 is highest priority
+    message: str
+
+    ERRORS = {
+        'F001': (1, 'output shorter than expected'),
+        'F002': (1, 'output longer then expected'),
+    }
+
+    @classmethod
+    def construct(cls, code: str, *args, **kwargs):
+        info = cls.ERRORS.get(code)
+        if info is None:
+            raise ValueError(f"Invalid error code {code!r}")
+        return cls(
+            code=code,
+            priority=info[0],
+            message=info[1].format(*args, **kwargs),
+        )
--- cptt/validate/token.py
+++ cptt/validate/token.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 import string
 from typing import Generator
 from typing import TextIO
+from itertools import zip_longest
 
+from cptt.error import TestingError
 from cptt.validate.base import Validator
 
+ErrorGenerator = Generator[TestingError, None, None]
 StrGenerator = Generator[str, None, None]
 
 
@@ -21,12 +24,15 @@ class TokenValidator(Validator):
 
         while c:
             if c in string.whitespace:
-                if tok:
-                    yield tok
+                if tok: yield tok
                 tok = ''
-            else:
-                tok += c
+            else: tok += c
             c = stream.read(1)
 
-        if tok:
-            yield tok
+        if tok: yield tok
+
+    def validate(self, output: TextIO, expected: TextIO) -> ErrorGenerator:
+        for outtok, exptok in zip_longest(
+                self.tokenize(output), self.tokenize(expected)):
+            if outtok is None: yield TestingError.construct('F001')
+            elif exptok is None: yield TestingError.construct('F002')
