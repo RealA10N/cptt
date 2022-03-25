24460d1d84421139bac9d8cf21abfb20c313fc04
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-26 01:22:58 +0300

    ✅ Added tokenize tests

---
+++ tests/test_tokenize.py
@@ -0,0 +1,29 @@
+import io
+from cptt.validate import TokenValidator
+
+
+def test_single_token():
+    assert list(TokenValidator.tokenize(io.StringIO('token'))) == ['token']
+
+
+def test_sentence():
+    tokens = list(TokenValidator.tokenize(io.StringIO('hello there!')))
+    assert tokens == ['hello', 'there!']
+
+
+def test_unicode():
+    tokens = list(TokenValidator.tokenize(io.StringIO('שלום עולם 😀🌍️')))
+    assert tokens == ['שלום', 'עולם', '😀🌍️']
+
+
+def test_sentence_with_whitespaces():
+    tokens = list(TokenValidator.tokenize(io.StringIO(' Hello\n There?  ')))
+    assert tokens == ['Hello', 'There?']
+
+
+def test_no_tokens():
+    assert list(TokenValidator.tokenize(io.StringIO(''))) == []
+
+
+def test_whitespaces_only():
+    assert list(TokenValidator.tokenize(io.StringIO(' \n \t'))) == []
