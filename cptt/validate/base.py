bb58c4f3060e902f130220881be73caa2e24b03c
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-26 01:20:18 +0300

    ✨ Added the `tokenize` method

--- cptt/validate/base.py
+++ cptt/validate/base.py
@@ -4,16 +4,14 @@ from typing import TextIO, Generator, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from cptt.error import TestingError
+    ErrorGenerator = Generator[TestingError, None, None]
 
 
 class Validator(ABC):
     """ Comperes the programs output with the expected one. """
 
     @abstractmethod
-    def validate(self,
-                 output: TextIO,
-                 expected: TextIO,
-                 ) -> Generator[TestingError, None, None]:
+    def validate(self, output: TextIO, expected: TextIO) -> ErrorGenerator:
         """ Comperes the programs output with the expected one. 'output' is the
         stream that the program writes it's output to. 'expected' is a stream
         that contains expected output that the validator uses as a reference
--- cptt/validate/token.py
+++ cptt/validate/token.py
@@ -1,5 +1,29 @@
+from __future__ import annotations
+import string
+from typing import TextIO, Generator
 from cptt.validate.base import Validator
+
+StrGenerator = Generator[str, None, None]
 
 
 class TokenValidator(Validator):
-    pass
+
+    @staticmethod
+    def tokenize(stream: TextIO) -> StrGenerator:
+        """ Consumes tokens (continuous words) from the given text stream and
+        yields them one after another. """
+
+        c = stream.read(1)
+        tok = ''
+
+        while c:
+            if c in string.whitespace:
+                if tok:
+                    yield tok
+                tok = ''
+            else:
+                tok += c
+            c = stream.read(1)
+
+        if tok:
+            yield tok
