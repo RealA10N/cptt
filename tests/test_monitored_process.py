fdacc109f96b78dac32865097e4286fa4478544b
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-30 00:05:21 +0300

    ✅ Added `MonitoredProcess` tests

---
+++ tests/test_monitored_process.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+import subprocess
+
+import pytest
+
+from cptt.process import MemoryLimitExceeded
+from cptt.process import MonitoredProcess
+from cptt.process import TimeLimitExceeded
+
+
+def requires_cli(name: str):
+    import shutil
+
+    return pytest.mark.skipif(
+        shutil.which(name) is None,
+        reason=f'required CLI {name!r} is not present',
+    )
+
+
+def python_script(code: str) -> list[str]:
+    """ Stores the given code in a temporary file, and returns the argument
+    list for executing the file with the running Python executable. """
+    import sys
+    import tempfile
+    import inspect
+
+    with tempfile.NamedTemporaryFile(
+        mode='w',
+            suffix='.py', encoding='utf8', delete=False,
+    ) as f:
+        f.write(inspect.cleandoc(code))
+
+    return (sys.executable, f.name)
+
+
+@requires_cli('echo')
+def test_echo():
+    process = MonitoredProcess(
+        ['echo', 'hi'],
+        encoding='utf8',
+        stdout=subprocess.PIPE,
+    )
+    process.wait()
+    assert process.stdout.read() == 'hi\n'
+
+
+def test_communicate_streams():
+    process = MonitoredProcess(
+        python_script("""
+        import sys
+        sys.stdout.write('stdout (:')
+        sys.stderr.write('stderr ):')
+        """),
+        encoding='utf8',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    out, err = process.communicate()
+    assert out == 'stdout (:'
+    assert err == 'stderr ):'
+
+
+def test_wait_streams():
+    process = MonitoredProcess(
+        python_script("""
+        import sys
+        sys.stdout.write('stdout (:')
+        sys.stderr.write('stderr ):')
+        """),
+        encoding='utf8',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    process.wait()
+    assert process.stdout.read() == 'stdout (:'
+    assert process.stderr.read() == 'stderr ):'
+
+
+def test_communicate_large_output():
+    process = MonitoredProcess(
+        python_script("""
+        for i in range(20_000):
+            print(i)
+        """),
+        encoding='utf8',
+        stdout=subprocess.PIPE,
+    )
+
+    out, _ = process.communicate()
+    assert len(out) > 100_000
+
+
+def test_memory_limit():
+    process = MonitoredProcess(
+        python_script("""
+        array = bytes(20_000_000)
+        print(array)
+        """),
+    )
+
+    with pytest.raises(MemoryLimitExceeded):
+        process.wait(time_limit=1, memory_limit=20_000_000)
+
+
+def test_time_limit():
+    process = MonitoredProcess(
+        python_script("""
+        import time
+        time.sleep(1)
+        """),
+    )
+
+    with pytest.raises(TimeLimitExceeded):
+        process.wait(time_limit=0.5, memory_limit=10_000_000)
