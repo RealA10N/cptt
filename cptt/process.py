0da2fc5af5086fadc3ab9d89e5374d1cc9ff636e
Alon (Ubuntu) <downtown2u@gmail.com>
2022-03-29 23:16:35 +0300

    ✨ Added `communicate` method for `MonitoredProcess`

--- cptt/process.py
+++ cptt/process.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
+import threading
 import time
+from typing import AnyStr
+from typing import IO
+from typing import Optional
 
 import psutil  # pip install psutil
 
@@ -56,8 +60,7 @@ class MonitoredProcess(psutil.Popen):
         Be aware that similarly to subprocess's `Popen.wait` method, if you are
         piping `stdout` or `stderr` and they are very large, the program might
         get into a deadlock state caused by the operating system. In that case,
-        you will have to use threads to read from the stream and free up space
-        in the buffer. """
+        use the `communicate` method instead. """
 
         start_time = time.time()
 
@@ -83,3 +86,56 @@ class MonitoredProcess(psutil.Popen):
                 pass
 
         return self.returncode
+
+    @staticmethod
+    def _consume_stream(stream: IO[AnyStr], to: list) -> None:
+        to.append(stream.read())
+        stream.close()
+
+    def communicate(
+        self,
+        input: AnyStr = None,
+        time_limit: float = None,
+        memory_limit: float = None,
+    ) -> tuple[Optional[AnyStr], Optional[AnyStr]]:
+        """ Comminicate with the process, and wait until it terminates.
+        Returns the buffered stdout and stderr stream values, after they
+        have been consumed and closed (if they were piped). """
+
+        if input:
+            self.stdin.write(input)
+            self.stdin.close()
+
+        threads: list[threading.Thread] = list()
+
+        if self.stdout is not None:
+            self._stdout_buf = list()
+            threads.append(
+                threading.Thread(
+                    target=self._consume_stream,
+                    args=(self.stdout, self._stdout_buf),
+                ),
+            )
+
+        if self.stderr is not None:
+            self._stderr_buf = list()
+            threads.append(
+                threading.Thread(
+                    target=self._consume_stream,
+                    args=(self.stderr, self._stderr_buf),
+                ),
+            )
+
+        for thread in threads:
+            thread.daemon = True
+            thread.start()
+
+        try:
+            self.wait(time_limit, memory_limit)
+        finally:
+            for thread in threads:
+                thread.join()
+
+        stdout = self._stdout_buf[0] if self.stdout else None
+        stderr = self._stderr_buf[0] if self.stderr else None
+        return (stdout, stderr)
